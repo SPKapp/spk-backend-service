1d08d47563b0a9782d7ff128b1a9c3c5680b847f
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2024-01-26 02:04:50 +0100

    Add e2e tests workflow

---
+++ test/common/region.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../src/app.module';
+import { DataSource, Repository } from 'typeorm';
+
+import { Region } from '../../src/common/modules/region/entities/region.entity';
+
+describe('Region tests', () => {
+  let app: INestApplication;
+  let dataSource: DataSource;
+  let regionRepository: Repository<Region>;
+
+  const regions: Region[] = [
+    {
+      id: 1,
+      name: 'Region 1',
+    },
+    {
+      id: 2,
+      name: 'Region 2',
+    },
+  ];
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+    dataSource = moduleFixture.get<DataSource>(DataSource);
+    regionRepository = dataSource.getRepository(Region);
+  });
+
+  it('should return list of regions', async () => {
+    await regionRepository.save(regions);
+
+    return request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `{ regions { id name } }`,
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.data.regions).toEqual(regions);
+      });
+  });
+});
