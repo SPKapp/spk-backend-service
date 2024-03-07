cded7b38de3904329d8689de950bc761f9bff612
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2024-03-07 14:11:51 +0100

    Add createUser mutation with role-based authorization and region ID validation

--- src/users/users/users.resolver.spec.ts
+++ src/users/users/users.resolver.spec.ts
@@ -1,20 +1,150 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException } from '@nestjs/common';
+
+import { FirebaseAuthGuard } from '../../common/modules/auth/firebase-auth/firebase-auth.guard';
+import { UserDetails } from '../../common/modules/auth/current-user/current-user';
+import { Role } from '../../common/modules/auth/roles.eum';
 
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
 
+import { CreateUserInput } from '../dto/create-user.input';
+import { User } from '../entities/user.entity';
+
 describe('UsersResolver', () => {
   let resolver: UsersResolver;
+  let usersService: UsersService;
+
+  const userDetailsTeplate: UserDetails = {
+    uid: '123',
+    email: 'email1@example.com',
+    phone: '123456789',
+    roles: [],
+    regions: [],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService],
-    }).compile();
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(FirebaseAuthGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     resolver = module.get<UsersResolver>(UsersResolver);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  describe('createUser', () => {
+    let createSpy: jest.SpyInstance;
+
+    const user: CreateUserInput = {
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'email1@example.com',
+      phone: '123456789',
+    };
+
+    beforeEach(() => {
+      createSpy = jest
+        .spyOn(usersService, 'create')
+        .mockImplementation(async () => new User(user));
+    });
+
+    it('should be defined', () => {
+      expect(resolver.createUser).toBeDefined();
+    });
+
+    describe('Admin', () => {
+      const userDetails: UserDetails = {
+        ...userDetailsTeplate,
+        roles: [Role.Admin],
+      };
+
+      it('should throw no region_id error', async () => {
+        await expect(resolver.createUser(userDetails, user)).rejects.toThrow(
+          new BadRequestException(
+            'Region ID is required for Admin and Region Manager with more than 1 region.',
+          ),
+        );
+        expect(createSpy).not.toHaveBeenCalled();
+      });
+
+      it('should create user', async () => {
+        const result = await resolver.createUser(userDetails, {
+          ...user,
+          region_id: 1,
+        });
+        expect(result).toEqual(new User(user));
+        expect(createSpy).toHaveBeenCalled();
+      });
+    });
+
+    describe('Region Manager', () => {
+      const userDetails: UserDetails = {
+        ...userDetailsTeplate,
+        roles: [Role.RegionManager],
+      };
+
+      it('should throw no region_id error', async () => {
+        await expect(
+          resolver.createUser({ ...userDetails, regions: [1, 2] }, user),
+        ).rejects.toThrow(
+          new BadRequestException(
+            'Region ID is required for Admin and Region Manager with more than 1 region.',
+          ),
+        );
+        expect(createSpy).not.toHaveBeenCalled();
+      });
+
+      it('should throw wrong region_id error', async () => {
+        await expect(
+          resolver.createUser(
+            { ...userDetails, regions: [2] },
+            { ...user, region_id: 1 },
+          ),
+        ).rejects.toThrow(
+          new BadRequestException(
+            'Region ID does not match the Region Manager permissions.',
+          ),
+        );
+        expect(createSpy).not.toHaveBeenCalled();
+      });
+
+      it('should create user - more than one region', async () => {
+        const result = await resolver.createUser(
+          { ...userDetails, regions: [1, 2] },
+          {
+            ...user,
+            region_id: 1,
+          },
+        );
+        expect(result).toEqual(new User(user));
+        expect(createSpy).toHaveBeenCalled();
+      });
+
+      it('should create user - one region', async () => {
+        const result = await resolver.createUser(
+          { ...userDetails, regions: [2] },
+          user,
+        );
+        expect(result).toEqual(new User(user));
+        expect(createSpy).toHaveBeenCalled();
+      });
+    });
+  });
 });
+
+// TODO: Add tests
--- src/users/users/users.resolver.ts
+++ src/users/users/users.resolver.ts
@@ -1,38 +1,80 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 
+import { FirebaseAuth } from '../../common/modules/auth/firebase-auth/firebase-auth.decorator';
+import { CurrentUser } from '../../common/modules/auth/current-user/current-user.decorator';
+import { UserDetails } from '../../common/modules/auth/current-user/current-user';
 import { UsersService } from './users.service';
+
 import { User } from '../entities/user.entity';
 import { CreateUserInput } from '../dto/create-user.input';
 import { UpdateUserInput } from '../dto/update-user.input';
+import { Role } from '../../common/modules/auth/roles.eum';
+import { BadRequestException } from '@nestjs/common';
 
 @Resolver(() => User)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
+  /**
+   * Creates a new user.
+   *
+   * @param user - The current user details.
+   * @param createUserInput - The input data for creating a user.
+   * @returns A Promise that resolves to the created user.
+   * @throws {BadRequestException} if the region ID is missing or invalid for the user's role.
+   */
+  @FirebaseAuth(Role.Admin, Role.RegionManager)
   @Mutation(() => User)
   async createUser(
+    @CurrentUser() user: UserDetails,
     @Args('createUserInput') createUserInput: CreateUserInput,
   ): Promise<User> {
-    // TODO: Add authentication
-    // TODO: Add authorization with role_id and team_id verification
+    if (
+      (user.roles.includes(Role.Admin) || user.regions.length > 1) &&
+      !createUserInput.region_id
+    ) {
+      throw new BadRequestException(
+        'Region ID is required for Admin and Region Manager with more than 1 region.',
+      );
+    }
+
+    if (
+      !user.roles.includes(Role.Admin) &&
+      user.roles.includes(Role.RegionManager)
+    ) {
+      if (!createUserInput.region_id) {
+        createUserInput.region_id = user.regions[0];
+      } else {
+        if (!user.regions.includes(createUserInput.region_id)) {
+          throw new BadRequestException(
+            'Region ID does not match the Region Manager permissions.',
+          );
+        }
+      }
+    }
+
     return await this.usersService.create(createUserInput);
   }
 
+  // TODO: Implement function
   @Query(() => [User], { name: 'users' })
   findAll() {
     return this.usersService.findAll();
   }
 
+  // TODO: Implement function
   @Query(() => User, { name: 'user' })
   async findOne(@Args('id', { type: () => Int }) id: number) {
     return await this.usersService.findOne(id);
   }
 
+  // TODO: Implement function
   @Mutation(() => User)
   updateUser(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
     return this.usersService.update(updateUserInput.id, updateUserInput);
   }
 
+  // TODO: Implement function
   @Mutation(() => User)
   removeUser(@Args('id', { type: () => Int }) id: number) {
     return this.usersService.remove(id);
