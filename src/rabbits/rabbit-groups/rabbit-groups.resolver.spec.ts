1a0dfaf328d37b471c66813458be63c5f230e188
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2024-03-13 22:33:38 +0100

    implement findOne rabbitGroup

--- src/rabbits/rabbit-groups/rabbit-groups.resolver.spec.ts
+++ src/rabbits/rabbit-groups/rabbit-groups.resolver.spec.ts
@@ -1,21 +1,142 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+
+import {
+  AuthService,
+  FirebaseAuthGuard,
+  Role,
+  UserDetails,
+  getCurrentUserPipe,
+} from '../../common/modules/auth/auth.module';
+
+import { Region } from '../../common/modules/regions/entities/region.entity';
+import { Team } from '../../users/entities/team.entity';
+
 import { RabbitGroupsResolver } from './rabbit-groups.resolver';
 import { RabbitGroupsService } from './rabbit-groups.service';
 
+import { RabbitGroup } from '../entities/rabbit-group.entity';
+
 describe('RabbitGroupsResolver', () => {
   let resolver: RabbitGroupsResolver;
+  let rabbitGroupsService: RabbitGroupsService;
+
+  const userDetailsTeplate: UserDetails = {
+    uid: '123',
+    email: 'email1@example.com',
+    phone: '123456789',
+    roles: [],
+    regions: [],
+  };
+
+  const rabbitGroup = new RabbitGroup({
+    id: 1,
+    region: new Region({ id: 1 }),
+    team: new Team({ id: 1 }),
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RabbitGroupsResolver, RabbitGroupsService],
-    }).compile();
+      providers: [
+        RabbitGroupsResolver,
+        AuthService,
+        {
+          provide: RabbitGroupsService,
+          useValue: {
+            findOne: jest.fn(() => rabbitGroup),
+          },
+        },
+      ],
+    })
+      .overridePipe(getCurrentUserPipe)
+      .useValue({ transform: jest.fn((currentUser) => currentUser) })
+      .overrideGuard(FirebaseAuthGuard)
+      .useValue({ canActivate: jest.fn(() => true) })
+      .compile();
 
     resolver = module.get<RabbitGroupsResolver>(RabbitGroupsResolver);
+    rabbitGroupsService = module.get<RabbitGroupsService>(RabbitGroupsService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
 
-  // TODO: Add tests
+  describe('findOne', () => {
+    it('should be defined', () => {
+      expect(resolver.findOne).toBeDefined();
+    });
+
+    it('should return a rabbit group', async () => {
+      const user = { ...userDetailsTeplate, roles: [Role.Admin] };
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).resolves.toEqual(
+        rabbitGroup,
+      );
+    });
+
+    it('should throw a NotFoundException if the rabbit group does not exist', async () => {
+      const user = { ...userDetailsTeplate, roles: [Role.Admin] };
+
+      jest.spyOn(rabbitGroupsService, 'findOne').mockResolvedValue(null);
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).rejects.toThrow(
+        new NotFoundException(
+          `Rabbit Group with ID ${rabbitGroup.id} not found`,
+        ),
+      );
+    });
+
+    it('should throw a ForbiddenException when the region manager does not have permissions', async () => {
+      const user = {
+        ...userDetailsTeplate,
+        roles: [Role.RegionManager],
+        regions: [2],
+      };
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).rejects.toThrow(
+        new ForbiddenException(
+          'Rabbit Group does not belong to the Region Manager permissions.',
+        ),
+      );
+    });
+
+    it('should return a rabbit group from the region manager', async () => {
+      const user = {
+        ...userDetailsTeplate,
+        roles: [Role.RegionManager],
+        regions: [1],
+      };
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).resolves.toEqual(
+        rabbitGroup,
+      );
+    });
+
+    it('should throw a ForbiddenException when the volunteer does not have permissions', async () => {
+      const user = {
+        ...userDetailsTeplate,
+        roles: [Role.Volunteer],
+        teamId: 2,
+      };
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).rejects.toThrow(
+        new ForbiddenException(
+          'Rabbit Group does not belong to the Volunteer permissions.',
+        ),
+      );
+    });
+
+    it('should return a rabbit group from the volunteer', async () => {
+      const user = {
+        ...userDetailsTeplate,
+        roles: [Role.Volunteer],
+        teamId: 1,
+      };
+
+      await expect(resolver.findOne(user, rabbitGroup.id)).resolves.toEqual(
+        rabbitGroup,
+      );
+    });
+  });
 });
--- src/rabbits/rabbit-groups/rabbit-groups.resolver.ts
+++ src/rabbits/rabbit-groups/rabbit-groups.resolver.ts
@@ -1,14 +1,81 @@
 import { Resolver, Query, Args, Int } from '@nestjs/graphql';
+import { ForbiddenException, NotFoundException } from '@nestjs/common';
+
+import {
+  AuthService,
+  FirebaseAuth,
+  Role,
+  CurrentUser,
+  UserDetails,
+} from '../../common/modules/auth/auth.module';
+
 import { RabbitGroupsService } from './rabbit-groups.service';
+
 import { RabbitGroup } from '../entities/rabbit-group.entity';
 
 @Resolver(() => RabbitGroup)
 export class RabbitGroupsResolver {
-  constructor(private readonly rabbitGroupsService: RabbitGroupsService) {}
+  constructor(
+    private readonly rabbitGroupsService: RabbitGroupsService,
+    private readonly authService: AuthService,
+  ) {}
 
+  /**
+   * Finds a rabbit group by its ID.
+   *
+   * @param currentUser - The current user details.
+   * @param id - The ID of the rabbit group to find.
+   * @returns A promise that resolves to the found rabbit group.
+   * @throws {NotFoundException} if the rabbit group with the specified ID is not found.
+   * @throws {ForbiddenException} if the current user does not have the required permissions.
+   */
+  @FirebaseAuth(Role.Admin, Role.RegionManager, Role.Volunteer)
   @Query(() => RabbitGroup, { name: 'rabbitGroup' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    // TODO: Implement this method
-    return this.rabbitGroupsService.findOne(id);
+  async findOne(
+    @CurrentUser('teamId') currentUser: UserDetails,
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<RabbitGroup> {
+    const ERROR_REGION_MANAGER =
+      'Rabbit Group does not belong to the Region Manager permissions.';
+    const ERROR_VOLUNTEER =
+      'Rabbit Group does not belong to the Volunteer permissions.';
+
+    let rabbitGroup: RabbitGroup | null = null;
+
+    if (currentUser.roles.includes(Role.Admin)) {
+      rabbitGroup = await this.rabbitGroupsService.findOne(id);
+      if (!rabbitGroup) {
+        throw new NotFoundException(`Rabbit Group with ID ${id} not found`);
+      }
+      return rabbitGroup;
+    }
+
+    await this.authService.checkRegionManagerPermissions(
+      currentUser,
+      async () => {
+        rabbitGroup = await this.rabbitGroupsService.findOne(id);
+        if (!rabbitGroup) {
+          throw new ForbiddenException(ERROR_REGION_MANAGER);
+        }
+        return rabbitGroup.region.id;
+      },
+      ERROR_REGION_MANAGER,
+    );
+
+    if (!rabbitGroup) {
+      await this.authService.checkVolunteerPermissions(
+        currentUser,
+        async () => {
+          rabbitGroup = await this.rabbitGroupsService.findOne(id);
+          if (!rabbitGroup) {
+            throw new ForbiddenException(ERROR_VOLUNTEER);
+          }
+          return rabbitGroup.team.id;
+        },
+        ERROR_VOLUNTEER,
+      );
+    }
+
+    return rabbitGroup;
   }
 }
