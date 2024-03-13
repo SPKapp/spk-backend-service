67ce5f2baef9be8a20070b87a03ab3c55a68ffe0
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2024-03-13 18:01:41 +0100

    Add Global decorator to Auth module

--- src/common/modules/auth/auth.module.ts
+++ src/common/modules/auth/auth.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { FirebaseAuthService } from './firebase-auth/firebase-auth.service';
 import { AuthService } from './auth.service';
 
+@Global()
 @Module({
   providers: [FirebaseAuthService, AuthService],
   exports: [FirebaseAuthService, AuthService],
