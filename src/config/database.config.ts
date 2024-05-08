4b2b06f5e85afc32591587f4444da3d99149e4ca
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2024-05-08 14:26:09 +0200

    add database ssl

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -18,6 +18,7 @@ export default registerAs(
           username: process.env.DATABASE_USERNAME,
           password: process.env.DATABASE_PASSWORD,
           database: process.env.DATABASE_NAME,
+          ssl: process.env.DATABASE_SSL === 'true',
           synchronize: process.env.NODE_ENV !== 'production',
           autoLoadEntities: true,
           logging:
